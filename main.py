172caf1751106225f45bd7984998e3a4492a02a7
Philou-X8 <pdurettesds@gmail.com>
2024-10-14 17:25:30 -0400

    merge and cleanup

--- main.py
+++ main.py
@@ -54,99 +54,82 @@ def rotate_image(image):
     return img_rot
 
 def freq_filter(image):
-
-    result = np.zeros([len(image),len(image[0])])
+    img_out = np.zeros([len(image),len(image[0])])
 
     b = [1 ,2 ,1]
     a = [2.3914,1.1072,0.5014]
 
-
-    result = signal.lfilter(b,a,image)
-    return result
+    img_out = signal.lfilter(b,a,image)
+    return img_out
 
 def freq_filter_with_python(image):
-    result = np.zeros([len(image),len(image[0])])
+    img_out = np.zeros([len(image),len(image[0])])
 
     wp = 500
     fs = 1600
-    order, _ = signal.buttord(wp,750,0.2,60,fs)
 
+    # buttord
+    order, _ = signal.buttord(wp,750,0.2,60,fs)
+    print('Butter : ' ,order)
     b,a = signal.butter(order,wp,fs=fs)
 
-    print('Butter : ' ,order)
-
-
+    # cheb1ord
     order, _ = signal.cheb1ord(wp,750,0.2,60,fs)
-
     print('Cheb1 : ' ,order)
 
+    # cheb2ord
     order, _ = signal.cheb2ord(wp,750,0.2,60,fs)
-
     print('Cheb2 : ' ,order)
 
-
+    # ellipord
     order, wn = signal.ellipord(wp,750,0.2,60,fs)
-
     print('Ellip : ' ,order)
-
     b,a = signal.ellip(order,rp=0.2,rs=60,Wn=wn,fs=fs)
 
-    result = signal.lfilter(b,a,image)
-
-
-    return result
+    img_out = signal.lfilter(b,a,image)
+    return img_out
 
 def freq_filter_auto(image):
-    f_sample = 1600 # sampling rate
-    f_pass = 500/(f_sample/2) # pass freq
-    f_stop = 750/(f_sample/2) # stop freq
-    td = 1
-    wp = (2/td)*np.tan(f_pass/2)
-    ws = (2/td)*np.tan(f_stop/2)
+    fs = 1600 # sampling rate
+    wp = 500 # pass freq
+    ws = 750 # stop freq
     gpass = 0.2
     gstop = 60
-    fs = 0
     n_order = 100
     b = []
     a = []
-    n_butter, wn_butter = signal.buttord(wp, ws, gpass, gstop)
+    n_butter, wn_butter = signal.buttord(wp, ws, gpass, gstop, fs=fs)
     print('n_butter: ' + str(n_butter))
     if n_butter < n_order:
         n_order = n_butter
-        b, a = signal.butter(n_butter, wn_butter, 'low')
+        b, a = signal.butter(n_butter, wp, 'low', fs=fs)
 
-    n_cheb1, wn_cheb1 = signal.cheb1ord(wp, ws, gpass, gstop)
+    n_cheb1, wn_cheb1 = signal.cheb1ord(wp, ws, gpass, gstop, fs=fs)
     print('n_cheb1: ' + str(n_cheb1))
     if n_cheb1 < n_order:
         n_order = n_cheb1
         filt_type = 'cheb1'
-        b, a = signal.cheby1(n_cheb1, wn_cheb1, 'low')
+        b, a = signal.cheby1(n_cheb1, gpass, wp, 'low', fs=fs)
 
-    n_cheb2, wn_cheb2 = signal.cheb2ord(wp, ws, gpass, gstop)
+    n_cheb2, wn_cheb2 = signal.cheb2ord(wp, ws, gpass, gstop, fs=fs)
     print('n_cheb2: ' + str(n_cheb2))
     if n_cheb2 < n_order:
         n_order = n_cheb2
         filt_type = 'cheb2'
-        b, a = signal.cheby1(n_cheb2, wn_cheb2, 'low')
+        b, a = signal.cheby2(n_cheb2, gstop, wp, 'low', fs=fs)
 
-    n_ellip, wn_ellip = signal.ellipord(wp, ws, gpass, gstop)
+    n_ellip, wn_ellip = signal.ellipord(wp, ws, gpass, gstop, fs=fs)
     print('n_ellip: ' + str(n_ellip))
     if n_ellip < n_order:
         n_order = n_ellip
         filt_type = 'ellip'
-        b, a = signal.cheby1(n_ellip, wn_ellip, 'low')
+        b, a = signal.ellip(n_ellip, gpass, gstop, wp, 'low', fs=fs)
 
-    # b, a = signal.butter(n_order, wn, 'low', output='ba')
-    img_filtered = signal.lfilter(b, a, image)
+
+    img_out = signal.lfilter(b, a, image)
     print(n_order)
-    #plt.plot(wn, )
 
-    plt.gray()
-    plt.imshow(image); plt.title('img original')
-    plt.show()
-    plt.imshow(img_filtered); plt.title('img filtered')
-    plt.show()
-    return img_filtered
+    return img_out
 
 
 if __name__ == '__main__':
@@ -162,7 +145,16 @@ if __name__ == '__main__':
     #img = img[0:200]
     img_cleaned = noise_remove(img)
     img_rotated = rotate_image(img_cleaned)
-    img_filtered = freq_filter(img_rotated)
+    img_filtered1 = freq_filter_with_python(img_rotated)
+    img_filtered = freq_filter_auto(img_rotated)
+
+    plt.gray()
+    plt.subplot(1, 2, 1); plt.title('Laurent filter')
+    plt.imshow(img_filtered1)
+    plt.subplot(1, 2, 2); plt.title('Philippe filter')
+    plt.imshow(img_filtered)
+    plt.show()
+
 
     plt.gray()
     plt.subplot(2, 3, 1); plt.title('source image')
@@ -172,7 +164,7 @@ if __name__ == '__main__':
     plt.subplot(2, 3, 3); plt.title('image rotated')
     plt.imshow(img_rotated)
     plt.subplot(2, 3, 4); plt.title('image high freq removed')
-    plt.imshow(img_denoise)
+    plt.imshow(img_filtered)
     plt.show()
 
 
